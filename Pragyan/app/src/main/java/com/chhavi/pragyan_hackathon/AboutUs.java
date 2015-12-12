5eab95ac31cee85824958a922e39cd37cf5c4c91
jig08 <grover.jigyasa1@gmail.com>
2015-12-12 18:52:34 +0530

    About Us layout and Java added.

---
+++ Pragyan/app/src/main/java/com/chhavi/pragyan_hackathon/AboutUs.java
@@ -0,0 +1,14 @@
+package com.chhavi.pragyan_hackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jigyasa on 12/12/15.
+ */
+public class AboutUs extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+    }
+}
