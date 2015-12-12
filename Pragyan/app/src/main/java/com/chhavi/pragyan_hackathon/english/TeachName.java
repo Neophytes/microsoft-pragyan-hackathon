160150d27d88379088ebf7360481c750861f63b0
chhavip <coolaqua12@gmail.com>
2015-12-12 13:28:44 +0530

    added take name functions

---
+++ Pragyan/app/src/main/java/com/chhavi/pragyan_hackathon/english/TeachName.java
@@ -0,0 +1,9 @@
+package com.chhavi.pragyan_hackathon.english;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chhavi on 12/12/15.
+ */
+public class TeachName extends AppCompatActivity{
+}
---
+++ Pragyan/app/src/main/java/com/chhavi/pragyan_hackathon/english/hello_screen.java
@@ -0,0 +1,89 @@
+package com.chhavi.pragyan_hackathon.english;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.chhavi.pragyan_hackathon.R;
+
+import java.util.Locale;
+
+/**
+ * Created by chhavi on 12/12/15.
+ */
+public class hello_screen extends AppCompatActivity implements
+        TextToSpeech.OnInitListener {
+    EditText take_name;
+    TextView display_name;
+    Button submit_button;
+    String name;
+    String textToSpeak;
+    TextToSpeech textToSpeech;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.take_name_layout);
+        submit_button = (Button)findViewById(R.id.submit_button);
+        display_name = (TextView)findViewById(R.id.display_name);
+        textToSpeech = new TextToSpeech(getApplicationContext(), this);
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                take_name = (EditText)findViewById(R.id.name_input);
+                if(!take_name.getText().equals("")){
+                    name = take_name.getText().toString();
+                    display_name.setText("Hello and welcome, "+name);
+                    textToSpeak = "Hello and welcome " + name;
+                    convertTextToSpeech(textToSpeak);
+                    Thread timer = new Thread() {
+                        public void run(){
+                            try {
+                               // buttons[inew][jnew].setBackgroundColor(Color.BLACK);
+                                sleep(5000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    };
+                    timer.start();
+                 //   Intent i = new Intent()
+
+
+
+                }
+            }
+        });
+
+    }
+    private void convertTextToSpeech(String text) {
+      //  String text = inputText.getText().toString();
+        if (null == text || "".equals(text)) {
+            text = "Please give some input.";
+        }
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("error", "This Language is not supported");
+            } else {
+                convertTextToSpeech(textToSpeak);
+            }
+        } else {
+            Log.e("error", "Initilization Failed!");
+        }
+    }
+}
